fdad32af0a38276539e29b12806ed7e6e216935c
BelenRaura <118556350+BelenRaura@users.noreply.github.com>
2024-08-13 19:00:38 -0500

    Add files via upload

---
+++ AmbatoWeather/AmbatoWeather/get-weather.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Define el path al entorno virtual si es necesario
+# source /home/belen/AmbatoWeather/AmbatoWeather/env/bin/activate
+
+# Define el path al script de Python
+SCRIPT_PATH="/home/belen/AmbatoWeather/AmbatoWeather/main.py"
+
+# Ejecuta el script de Python
+python3 "$SCRIPT_PATH"
